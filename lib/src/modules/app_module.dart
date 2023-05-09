713c0608c510783ab0721cdd88f2059d66a64bec
kleytonsouza <kleytonl.souza@gmail.com>
2023-05-09 16:30:08 -0300

    add files

---
+++ lib/src/modules/app_module.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import './home/home_page.dart';
+
+class AppModule extends Module {
+  @override
+  List<Bind> get binds => [];
+
+  @override
+  List<ModularRoute> get routes => [
+        ChildRoute('/', child: (context, args) => const HomePage()),
+      ];
+}
